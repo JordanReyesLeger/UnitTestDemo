6eaae8f5d1e02a0063a5b7372b1582fbbc4c0210
Jordan Reyes Leger <cuervorl@microsoft.com>
2020-03-24 13:32:07 -0600

    Adicioando prueba unitaria

---
+++ SolutionConsolteTest/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65241e79-108a-46df-b17c-d5dab7128f08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SolutionConsolteTest/UnitTestProject/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void MetodoLargo()
+        {
+            Thread.Sleep(new TimeSpan(0, 2, 0));
+        }
+        [TestMethod]
+        public void MetodoMediano()
+        {
+            Thread.Sleep(new TimeSpan(0, 1, 0));
+        }
+
+        [TestMethod]
+        public void MetodoCorto()
+        {
+            Thread.Sleep(new TimeSpan(0, 0, 30));
+        }
+    }
+}
